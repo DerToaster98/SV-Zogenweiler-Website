bb355b57374a1885896f81d0bba07c6f18620059
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-10-04 17:28:12 +0200

    different structure

--- sv-zogenweiler-website/src/App.js
+++ sv-zogenweiler-website/src/App.js
@@ -3,15 +3,19 @@ import './App.css';
 import { HashRouter, Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/pages/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <HashRouter basename="/">
         <Header></Header>
-        <Routes>
-          <Route path ='/' element={<Home></Home>}/>            
-        </Routes>
+        <body>
+          <Routes>
+            <Route path ='/' element={<Home></Home>}/>            
+          </Routes>
+        </body>
+        <Footer></Footer>
       </HashRouter>
     </div>
   );
