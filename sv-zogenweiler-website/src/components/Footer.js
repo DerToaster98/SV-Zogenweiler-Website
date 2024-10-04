6a6214e06fad4cdd55ea76e998faf4f2b0c97797
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-10-04 17:28:33 +0200

    add footer

---
+++ sv-zogenweiler-website/src/components/Footer.js
@@ -0,0 +1,21 @@
+import { Component } from "react";
+import { NavLink } from 'react-router-dom';
+import "./footer.css";
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <footer className="pageFooter color-green-footer">           
+                <nav className="navbox-footer-right">                            
+                    <span className="navbarItemFooter">Schützenverein Hubertus Zogenweiler e.V.</span>
+                    <NavLink className="navbarItemFooter" activeClassname="navbarItemFooter active" to="/impressum">
+                        Impressum
+                    </NavLink>                                
+                    <NavLink className="navbarItemFooter" activeClassname="navbarItem active" to="/datenschutz">                                    
+                        Datenschutz
+                    </NavLink>                    
+                </nav>
+            </footer>
+        );
+    }
+}
