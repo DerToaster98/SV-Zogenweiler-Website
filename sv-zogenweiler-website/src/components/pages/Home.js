944ee6bf11c3d65c750b2811c0daf1d9af1827a5
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-11-12 20:46:07 +0100

    more basic css and more home page

--- sv-zogenweiler-website/src/components/pages/Home.js
+++ sv-zogenweiler-website/src/components/pages/Home.js
@@ -8,28 +8,36 @@ import './home.css';
 export default class Home extends Component {
     render() {
         return(
-            <div className="home page-content">
-                <header>
-                    <h2>Startseite</h2>
-                </header>
-                <article>
-                    <div className="text-side-by-side">
-                        <div className="image-container">
-                            <img alt="SV-Logo" src={logo} className="logo-home-text"></img>
-                        </div>    
-                        <div>
-                            <span>123</span>
-                            <br/>
-                            <span>123</span>
-                            <br/>
-                            <span>123</span>
-                            <br/>
+            <div className="home">
+                <div className="page-content">
+                    <header>
+                        <h1>Startseite</h1>
+                    </header>
+                    <article>
+                        <div className="text-side-by-side">
+                            <div className="image-container">
+                                <img alt="SV-Logo" src={logo} className="logo-home-text"></img>
+                            </div>    
+                            <div>
+                                <span>123</span>
+                                <br/>
+                                <span>123</span>
+                                <br/>
+                                <span>123</span>
+                                <br/>
+                            </div>
                         </div>
+                    </article>
+                </div>
+                <div className="green-bar-background">
+                    <div className="page-content">
+                        <header>
+                            <h2>Trainingszeiten</h2>
+                        </header>
+                        <article>
+                        </article>
                     </div>
-                </article>
-                <footer>
-
-                </footer>
+                </div>
             </div>
         );
     }
