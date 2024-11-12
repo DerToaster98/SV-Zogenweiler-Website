e7545cdf6a53321ba8bf38eda95eba3691602e26
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-11-12 20:28:43 +0100

    begin home page

--- sv-zogenweiler-website/src/components/pages/Home.js
+++ sv-zogenweiler-website/src/components/pages/Home.js
@@ -1,10 +1,35 @@
 import { Component } from "react";
 
+import 'react-slideshow-image/dist/styles.css';
+import logo from "../../assets/Logo_SV_Zogenweiler.png";
+
+import './home.css';
+
 export default class Home extends Component {
     render() {
         return(
-            <div className="home">
-                
+            <div className="home page-content">
+                <header>
+                    <h2>Startseite</h2>
+                </header>
+                <article>
+                    <div className="text-side-by-side">
+                        <div className="image-container">
+                            <img alt="SV-Logo" src={logo} className="logo-home-text"></img>
+                        </div>    
+                        <div>
+                            <span>123</span>
+                            <br/>
+                            <span>123</span>
+                            <br/>
+                            <span>123</span>
+                            <br/>
+                        </div>
+                    </div>
+                </article>
+                <footer>
+
+                </footer>
             </div>
         );
     }
