20a756007b8d35c40d26248516ceb3071f6fc9be
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-12-28 15:51:14 +0100

    add more constants we need

--- sv-zogenweiler-website/src/Constants.js
+++ sv-zogenweiler-website/src/Constants.js
@@ -1,4 +1,14 @@
 export const SITE_BASE_URL = "TBD";
+
+export const ADDRESS = "Görtbild 6, 88263 Horgenzell";
+
+export const GOOGLE_MAPS_API_KEY = "GOOGLE MAPS API KEY";
+
+export const HOUSE_COORDINATES = {
+    lat: 47.837921, 
+    lng: 9.493331
+};
+
 export function getSiteBaseURL() {
     return window.location.origin + "/" + SITE_BASE_URL;
 }
