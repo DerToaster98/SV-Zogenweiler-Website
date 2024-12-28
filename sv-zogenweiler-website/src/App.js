d1eabd4fdd95cc23cd962a8fbebc066a6138e759
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2024-12-28 15:51:52 +0100

    include Contacts page

--- sv-zogenweiler-website/src/App.js
+++ sv-zogenweiler-website/src/App.js
@@ -4,15 +4,17 @@ import { HashRouter, Route, Routes} from 'react-router-dom';
 import Header from './components/Header';
 import Home from './components/pages/Home';
 import Footer from './components/Footer';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App color-gray-background">
       <HashRouter basename="/">
         <Header></Header>
         <body>
           <Routes>
-            <Route path ='/' element={<Home></Home>}/>            
+            <Route path ='/' element={<Home></Home>}/>
+            <Route path='/kontakt' element={<Contact></Contact>}/>
           </Routes>
         </body>
         <Footer></Footer>
