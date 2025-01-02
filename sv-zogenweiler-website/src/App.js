351bec452b46cf2590601ad6aa67b65f124006a8
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-01-02 18:15:00 +0100

    link new pages

--- sv-zogenweiler-website/src/App.js
+++ sv-zogenweiler-website/src/App.js
@@ -5,20 +5,26 @@ import Header from './components/Header';
 import Home from './components/pages/Home';
 import Footer from './components/Footer';
 import Contact from './components/pages/Contact';
+import Club from './components/pages/club/Club';
+import Chiefs from './components/pages/club/Chiefs';
 
 function App() {
-  return (
-    <div className="App color-gray-background">
-      <HashRouter basename="/">
-        <Header></Header>
-        <body>
-          <Routes>
-            <Route path ='/' element={<Home></Home>}/>
-            <Route path='/kontakt' element={<Contact></Contact>}/>
-          </Routes>
-        </body>
-        <Footer></Footer>
-      </HashRouter>
+  return (    
+      <div id="root" className="App color-gray-background">       
+        <HashRouter basename="/">
+          <Header></Header>          
+          <div className='main-content'>
+            <Routes>
+              <Route path ='/' element={<Home></Home>}/>
+              <Route path='kontakt' element={<Contact></Contact>}/>
+              <Route path='verein' element={<Club></Club>}/>
+              <Route path='verein/vorstandschaft' element={<Chiefs></Chiefs>}/>
+            </Routes>                   
+          </div>
+          <footer>
+            <Footer className='main-footer'></Footer>
+          </footer>
+        </HashRouter>              
     </div>
   );
 }
