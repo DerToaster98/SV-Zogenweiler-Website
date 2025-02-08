a596956a144e4d63a8febecf9bf202b18f1d19e6
DerToaster98 <38782719+DerToaster98@users.noreply.github.com>
2025-02-08 16:42:43 +0100

    add utility functions for contacts and addresses

--- sv-zogenweiler-website/src/Constants.js
+++ sv-zogenweiler-website/src/Constants.js
@@ -12,3 +12,107 @@ export const HOUSE_COORDINATES = {
 export function getSiteBaseURL() {
     return window.location.origin + "/" + SITE_BASE_URL;
 }
+
+export function getClubHouseAddress() {
+    return (
+        <dl>
+            <dt>
+                <span className="bold-text">
+                    Schützenhaus Zogenweiler
+                </span>
+            </dt>
+            <dt>
+                <span>
+                    Görtbild 6
+                </span>
+            </dt>
+            <dt>
+                <span>
+                    88263 Horgenzell   
+                </span>
+            </dt>
+            <dt><br></br></dt><br></br>
+            <dt>
+                <span>
+                    Tel.: 07504 - 1761
+                </span>
+            </dt>
+            <dt>
+                <span>
+                    (Nur während Öffnungszeiten)
+                </span>
+            </dt>
+        </dl>       
+    );
+}
+
+export function getResponsiblePersonAddress() {
+    return (
+        <dl>            
+            <dt>
+                <span>
+                    Rolf Wiggenhauser   
+                </span>
+            </dt>
+            <dt>
+                <span>
+                    Bettenweiler   
+                </span>
+            </dt>
+            <dt>
+                <span>
+                    88263 Horgenzell   
+                </span>
+            </dt>
+            <dt><br></br></dt>                                    
+            <dt>
+                <span>
+                    Tel.: 07504 - 1822   
+                </span>
+            </dt>
+            <dt>
+                <span>
+                    <a href="mailto:osm@svzogenweiler.de">osm@svzogenweiler.de   </a>
+                </span>
+            </dt>
+        </dl>  
+    );
+}
+
+export function getOSMAddress() {
+    return (
+        <dl>
+            <dt>
+                <span className="bold-text">
+                    Oberschützenmeister
+                </span>
+            </dt>
+            <dt>
+                <span>
+                    Rolf Wiggenhauser   
+                </span>
+            </dt>
+            <dt>
+                <span>
+                    Bettenweiler   
+                </span>
+            </dt>
+            <dt>
+                <span>
+                    88263 Horgenzell   
+                </span>
+            </dt>
+            <dt><br></br></dt>                                    
+            <dt>
+                <span>
+                    Tel.: 07504 - 1822   
+                </span>
+            </dt>
+            <dt>
+                <span>
+                    <a href="mailto:osm@svzogenweiler.de">osm@svzogenweiler.de   </a>
+                </span>
+            </dt>
+        </dl>  
+    );
+}
